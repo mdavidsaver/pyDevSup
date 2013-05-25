e72bd9b1aa4ee2bad0d60678cbda3cf775b549ca
Michael Davidsaver <mdavidsaver@bnl.gov>
2013-05-25 12:55:26 -0400

    build doc with sphinx 0.6.6

--- documentation/conf.py
+++ documentation/conf.py
@@ -21,11 +21,11 @@ import sys, os
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+#needs_sphinx = '0.6'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

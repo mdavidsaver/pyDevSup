e69f32474dbbd0e0c33cd59f99bbc2b93a86f006
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-02-17 16:17:02 -0500

    more ArchiveEngine monitor

--- iocBoot/iocarchivemon/st.cmd
+++ iocBoot/iocarchivemon/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linux-x86_64/softIocPy2.6
+#!../../bin/linux-x86/softIocPy2.7
 
 < envPaths
 
@@ -11,7 +11,12 @@ py "logging.basicConfig(level=logging.INFO)"
 epicsEnvSet("BASE","/var/cache/channelarchiver")
 epicsEnvSet("PPAT","The original process ID was ([0-9]+)")
 
-dbLoadRecords("../../db/pidmon.db","N=ACC-CT{Bck}General-I,SCAN=10 second,FILE=$(BASE)/general/archive_active.lck,PAT=$(PAT)")
+dbLoadRecords("../../db/pidmon.db","N=ACC-CT{Bck}Diag-I,SCAN=10 second,FILE=$(BASE)/diag/archive_active.lck,PAT=$(PPAT)")
+dbLoadRecords("../../db/pidmon.db","N=ACC-CT{Bck}Gen-I,SCAN=10 second,FILE=$(BASE)/general/archive_active.lck,PAT=$(PPAT)")
+dbLoadRecords("../../db/pidmon.db","N=ACC-CT{Bck}InjVA-I,SCAN=10 second,FILE=$(BASE)/inj-va/archive_active.lck,PAT=$(PPAT)")
+dbLoadRecords("../../db/pidmon.db","N=ACC-CT{Bck}PSCrit-I,SCAN=10 second,FILE=$(BASE)/PS_crit/archive_active.lck,PAT=$(PPAT)")
+dbLoadRecords("../../db/pidmon.db","N=ACC-CT{Bck}RadMon-I,SCAN=10 second,FILE=$(BASE)/RadMon/archive_active.lck,PAT=$(PPAT)")
+dbLoadRecords("../../db/pidmon.db","N=ACC-CT{Bck}Skids-I,SCAN=10 second,FILE=$(BASE)/waterskid/archive_active.lck,PAT=$(PPAT)")
 
 iocInit()
 

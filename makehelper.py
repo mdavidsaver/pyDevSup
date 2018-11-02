1d8c6ab7b5cb837f8aa5f2d5ae8976ffa3c88c26
Michael Davidsaver <mdavidsaver@gmail.com>
2018-11-02 16:47:57 -0700

    Makefile updates from p4p

--- makehelper.py
+++ makehelper.py
@@ -28,22 +28,38 @@ libdirs = [get_config_var('LIBDIR')]
 have_np='NO'
 try:
     from numpy.distutils.misc_util import get_numpy_include_dirs
-    incdirs += get_numpy_include_dirs()
+    incdirs = get_numpy_include_dirs()+incdirs
     have_np='YES'
 except ImportError:
     pass
-
-incdirs = [get_python_inc()]+get_numpy_include_dirs()
-libdirs = [get_config_var('LIBDIR')]
 
 print('TARGET_CFLAGS +=',get_config_var('BASECFLAGS'), file=out)
 print('TARGET_CXXFLAGS +=',get_config_var('BASECFLAGS'), file=out)
 
 print('PY_VER :=',get_config_var('VERSION'), file=out)
-print('PY_LD_VER :=',get_config_var('LDVERSION') or get_config_var('VERSION'), file=out)
+ldver = get_config_var('LDVERSION')
+if ldver is None:
+    ldver = get_config_var('VERSION')
+    if get_config_var('Py_DEBUG'):
+        ldver = ldver+'_d'
+print('PY_LD_VER :=',ldver, file=out)
 print('PY_INCDIRS :=',' '.join(incdirs), file=out)
 print('PY_LIBDIRS :=',' '.join(libdirs), file=out)
 print('HAVE_NUMPY :=',have_np, file=out)
+
+try:
+    import asyncio
+except ImportError:
+    print('HAVE_ASYNCIO := NO', file=out)
+else:
+    print('HAVE_ASYNCIO := YES', file=out)
+
+try:
+    import cothread
+except ImportError:
+    print('HAVE_COTHREAD := NO', file=out)
+else:
+    print('HAVE_COTHREAD := YES', file=out)
 
 print('PY_OK := YES', file=out)
 

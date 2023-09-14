2fcf37aa8a58d7bdba18c9e2e93a90da30597653
Peter Heesterman <Peter.Heesterman@Capgemini.com>
2023-09-14 17:17:02 +0100

    Added use of the PY_LDLIBS variable to locate the Python import library. On Windows, the library isn't in a system folder so the fully-qualified path is required.

--- makehelper.py
+++ makehelper.py
@@ -58,7 +58,10 @@ if ldver is None:
 print('PY_LD_VER :=',ldver, file=out)
 print('PY_INCDIRS :=',' '.join(incdirs), file=out)
 print('PY_LIBDIRS :=',' '.join(libdirs), file=out)
-print('PY_LDLIBS :=', get_config_var('BLDLIBRARY') or '', file=out)
+if sys.platform == 'win32':
+    print('PY_LDLIBS :=', os.path.join(sys.prefix, 'libs') + '\python'+ldver+'.lib', file=out)
+else:
+    print('PY_LDLIBS :=', get_config_var('BLDLIBRARY') or '', file=out)
 print('HAVE_NUMPY :=',have_np, file=out)
 
 try:

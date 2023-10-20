dd2c1ecbb2846d2f3e2ce3f0bd62b3e8df943352
Peter Heesterman <Peter.Heesterman@Capgemini.com>
2023-10-20 15:20:10 +0100

    Try pushd / popd instead of set PYTHONPATH.

--- devsupApp/src/devsup/__init__.py
+++ devsupApp/src/devsup/__init__.py
@@ -7,10 +7,12 @@ if sys.platform == 'win32':
     # See https://stackoverflow.com/questions/72858093/how-to-specify-pyd-dll-dependencies-search-paths-at-runtime-with-python
     # This is required for use of e.g. nose testing, but
     # not when running as an IOC, since the IOC will already have loaded EPICS base DLLs.
+    xepics_base = None
     xepics_base = os.getenv('XEPICS_BASE')
-    print("xepics_base = " + xepics_base)
+    print("xepics_base = " + str(xepics_base))
+    epics_host_arch = None
     epics_host_arch = os.getenv('EPICS_HOST_ARCH')
-    print("epics_host_arch = " + epics_host_arch)
+    print("epics_host_arch = " + str(epics_host_arch))
     if xepics_base is not None and epics_host_arch is not None:
         os.add_dll_directory(xepics_base.strip() + "/bin/" + epics_host_arch)
 
